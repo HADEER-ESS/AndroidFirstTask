00cc4af77784a73d0e31f48a8ed15721a5e6e18e
hadeer <hadeeressam21@gmail.com>
2024-07-16 23:58:43 +0300

    create data class for post items, and create array data to fetch it and display on screen

---
+++ app/src/main/java/com/example/task4androidcourse/data/Post.kt
@@ -0,0 +1,9 @@
+package com.example.task4androidcourse.data
+
+data class Post(
+    val id : Int,
+    val userName : String,
+    val content : String,
+    val like : Int,
+    val share : Int
+)
